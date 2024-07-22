cf370e0c7c0686703a82658ca3bf9720baff2656
Reyes-Rivera <143473424+Reyes-Rivera@users.noreply.github.com>
2024-07-22 13:59:22 -0600

    backend cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,7 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:5173', 
+    origin: 'https://bejewelled-flan-378a6d.netlify.app', 
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true, 
   });
